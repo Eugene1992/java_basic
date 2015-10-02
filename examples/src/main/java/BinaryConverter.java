5221d3c5f40debe1ccf83ce33ca6a36206a7154f
Eugene <deyneko55@gmail.com>
2015-10-02 16:36:57 +0300

    initial commit

---
+++ examples/src/main/java/BinaryConverter.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Евгений on 02.10.2015.
+ */
+public class BinaryConverter {
+    public static void main(String[] args) {
+        int value = 120;
+        int temp;
+        int one, two, three, four, five, six, seven, eight;
+        one = value % 2;
+        temp = value / 2;
+        two = temp % 2;
+        temp = temp / 2;
+        three = temp % 2;
+        temp = temp / 2;
+        four = temp % 2;
+        temp = temp / 2;
+        five = temp % 2;
+        temp = temp / 2;
+        six = temp % 2;
+        temp = temp / 2;
+        seven = temp % 2;
+        temp = temp / 2;
+        eight = temp % 2;
+
+        System.out.println(eight + "" + seven + "" + six + "" + five + "" + four + "" + three + "" + two + "" + one);
+
+    }
+}
