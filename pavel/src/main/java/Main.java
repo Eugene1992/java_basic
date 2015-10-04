e2efb7aeb4e8aff740ec5e5cf386b53a247a027e
potanin.p <Jhkfylj325>
2015-10-04 22:27:27 +0300

    Додано виконання завдання

--- pavel/src/main/java/Main.java
+++ pavel/src/main/java/Main.java
@@ -15,30 +15,37 @@ public class Main {
 
         //Инициализировать переменную типа byte результатом суммы двух любых чисел
         a = 1+1; // 2
+        System.out.println("Инициализировать переменную типа byte результатом суммы двух любых чисел");
         System.out.println(a);
 
         //Инициализировать переменную типа short  результатом вычитания двух любых чисел
         b = 4-2; // 2
+        System.out.println("Инициализировать переменную типа short  результатом вычитания двух любых чисел");
         System.out.println(b);
 
         //Инициализировать переменную типа int результатом деления двух любых чисел
         c = 3 / 3; // 1
+        System.out.println("Инициализировать переменную типа int результатом деления двух любых чисел");
         System.out.println(c);
 
         //Инициализировать переменную типа long результатом умножения двух любых чисел
         d = 3000 * 3000; // 9000000
+        System.out.println("Инициализировать переменную типа long результатом умножения двух любых чисел");
         System.out.println(d);
 
         //Передать перменной типа byte результат суммы двух переменной типа byte
         a=(byte) (a+a); // 4
+        System.out.println("Передать перменной типа byte результат суммы двух переменной типа byte");
         System.out.println(a);
 
         //Передать переменной типа byte результат суммы двух переменных типа short, деленных на третью переменную типа byte
         a = (byte) (b+b/a); // 2+2/2=1
+        System.out.println("Передать переменной типа byte результат суммы двух переменных типа short, деленных на третью переменную типа byte");
         System.out.println(a);
 
         //Переменной типа int передать результат деления двух short, умноженных на результат деления двух byte
         c = (b/b*a);
+        System.out.println("Переменной типа int передать результат деления двух short, умноженных на результат деления двух byte");
         System.out.println(c);
 
         //Поменять значения двух целочисленных переменных используя третью
@@ -48,18 +55,43 @@ public class Main {
 
         k1 = 11;
         k2 = 65;
-
+        System.out.println("Число к1 до конвертации");
         System.out.println(k1);
+        System.out.println("Число к2 до конвертации");
         System.out.println(k2);
 
         k3 = k1;
         k1 = k2;
         k2 = k3;
 
+        System.out.println("Число к1 после конвертации");
         System.out.println(k1);
+        System.out.println("Число к2 после конвертации");
         System.out.println(k2);
 
-        
+        //Поменять значения двух переменных без использования третей
+        int a1;
+        int b1;
+
+        a1 = 1;
+        b1 = 2;
+        System.out.println("Число а1");
+        System.out.println(a1);
+        System.out.println("Число b1");
+        System.out.println(b1);
+
+        a1 = b1+a1;
+        b1 = a1-b1;
+        a1 = a1-b1;
+
+        System.out.println("Результат а1");
+        System.out.println(a1);
+        System.out.println("Результат b1");
+        System.out.println(b1);
+
+
+
+
     }
 
 }
