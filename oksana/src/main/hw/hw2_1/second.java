7f9fda7ffbd411e1f0bc7174623f97e5eaec29a3
OksanaMikolenko <kseniamikolenko@outlook.com>
2015-10-07 18:18:56 +0200

    hw

---
+++ oksana/src/main/hw/hw2_1/second.java
@@ -0,0 +1,15 @@
+package hw2_1;
+
+/**
+ * Created by Оксана on 07.10.2015.
+ */
+public class second {
+    public static void main(String[] args) {
+        double x,y,z;
+        x=1;
+        y=2;
+        z=4;
+        double av=(x+y+z)/3;
+        System.out.println(av);
+    }
+}
