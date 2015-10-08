d76d67ce19bb2d3f8d2fb3aaaba2007e78bd7668
potanin.p <Jhkfylj325>
2015-10-08 20:51:48 +0300

    Додано частину ДЗ hw2

---
+++ pavel/src/main/hw/hw1/arithmeticAverage.java
@@ -0,0 +1,25 @@
+package hw1;
+
+/**
+ * Created by ppota on 10/8/2015.
+ */
+
+//Задание 2
+//        Используя IntelliJ IDEA, создайте класс arithmeticAverage .
+//        Вычислите среднее арифметическое трех целочисленных значений и выведите его на экран.
+//        С какой проблемой вы столкнулись? Какой тип переменных лучше использовать для корректного отображения результата?
+
+public class arithmeticAverage {
+    public static void main(String[] args) {
+        int a = 144;
+        int b = 10;
+        int c = 11;
+        int d;
+
+        d = (a + b + c)/3;
+        System.out.println(d);
+
+
+    }
+}
+
