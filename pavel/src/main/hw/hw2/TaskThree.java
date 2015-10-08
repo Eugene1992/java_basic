116579a760271b45f667ca269ce345e22cfed917
potanin.p <Jhkfylj325>
2015-10-08 21:42:18 +0300

    Додано частину ДЗ hw3          Задание 2

--- pavel/src/main/hw/hw2/TaskThree.java
+++ pavel/src/main/hw/hw2/TaskThree.java
@@ -14,7 +14,7 @@ public class TaskThree {
         int b = 4;
         int c = 3;
         int e = 2;
-        double d = a + b/c/e;
+        double d = (float)a + ((float)b/c/e);
         System.out.println(d);
     }
 }
