b042b854381d2c1fd8ddea0798bace980bd9314a
potanin.p <Jhkfylj325>
2015-10-08 20:58:19 +0300

    Додано частину ДЗ hw2 Задание 3

---
+++ pavel/src/main/hw/hw1/circle.java
@@ -0,0 +1,26 @@
+package hw1;
+
+/**
+ * Created by ppota on 10/8/2015.
+ */
+
+
+//Задание 3
+//        Используя IntelliJ IDEA, создайте класс circle.
+//        Создайте константу с именем PI (число π «пи»), создайте переменную радиус с именем – r. Используя формулу πR2, вычислите площадь круга и выведите результат на экран.
+
+public class circle {
+    public static void main (String[] args){
+        double r =10;
+        final double Pi = 3.1415926536;
+        r = Pi * r;
+        System.out.println (r);
+
+
+
+
+
+
+
+    }
+}
