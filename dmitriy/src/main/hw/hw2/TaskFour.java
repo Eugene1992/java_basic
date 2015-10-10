df66e7e78643b3e90bc5514c475662b38a8f1248
Bokhanchenko <Bokhanchenko.dm@gmail.com>
2015-10-10 10:52:58 +0300

    I finished

--- dmitriy/src/main/hw/hw2/TaskFour.java
+++ dmitriy/src/main/hw/hw2/TaskFour.java
@@ -12,7 +12,7 @@ public class TaskFour {
     public static void main(String[] args) {
         int a = 15;
         int b = 4;
-        float c = a / b;
+        float c = (float)a / b;
         double d = a * 1e-3 + c;
         System.out.println(d);
     }
