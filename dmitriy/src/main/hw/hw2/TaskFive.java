1658319d0f9853881a70f01769cf799af546ed65
Bokhanchenko <Bokhanchenko.dm@gmail.com>
2015-10-10 10:56:06 +0300

    I finished

--- dmitriy/src/main/hw/hw2/TaskFive.java
+++ dmitriy/src/main/hw/hw2/TaskFive.java
@@ -12,7 +12,7 @@ public class TaskFive {
     public static void main(String[] args) {
         float f = (float)128.50;
         int i = (int)f;
-        int b = (int)(i + f);
+        int b = (byte)(i + f);
         System.out.println(b);
     }
 }
