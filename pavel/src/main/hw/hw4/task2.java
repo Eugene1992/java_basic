cbb955f9c1ffc8269a812fbd8331936cbaf8125c
potanin.p <Jhkfylj325>
2015-10-11 21:14:41 +0300

    Додано частину ДЗ hw4   Задание 2

---
+++ pavel/src/main/hw/hw4/task2.java
@@ -0,0 +1,19 @@
+package hw4;
+
+/**
+ * Created by ppota on 10/11/2015.
+ */
+//2. Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….
+public class task2 {
+    public static void main(String[] args) {
+        {
+            for (int i = 1; i <= 55;i++) {
+                if ((i % 2) == 0) ;
+                    else
+                    System.out.println(i);
+
+            }
+        }
+    }
+}
+
