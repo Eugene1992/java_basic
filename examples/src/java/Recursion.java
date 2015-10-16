891678026ea4d3abaa6a8a12a59bb8511e0f5f92
Eugene <deyneko55@gmail.com>
2015-10-16 17:55:09 +0300

    hw5

---
+++ examples/src/java/Recursion.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Евгений on 16.10.2015.
+ */
+public class Recursion {
+    public static void main(String[] args) {
+        print(0);
+    }
+
+    static void print(int k) {
+        //System.out.println(k);
+        if (k < 5) {
+            print(k+1);
+        }
+        System.out.println(k);
+    }
+
+}
