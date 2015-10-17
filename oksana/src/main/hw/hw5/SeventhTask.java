cbab330e200bf9e1424faf02dc32be5f964ca181
OksanaMikolenko <kseniamikolenko@outlook.com>
2015-10-17 11:19:57 +0200

    hw

--- oksana/src/main/hw/hw5/SeventhTask.java
+++ oksana/src/main/hw/hw5/SeventhTask.java
@@ -7,26 +7,28 @@ import static java.lang.Math.min;
 
 /**
  * Created by Оксана on 16.10.2015.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 7. Найти минимальное и максимальное число в массиве
  */
 public class SeventhTask {
     public static void main(String[] args) {
-        Scanner read = new Scanner( System.in);
-        int size=read.nextInt();
-        int[] arr=new int[size];
-        for (int i=0; i<size;++i)
-            arr[i]=read.nextInt();
-        int mn,mx;
-        mn=mx=arr[0];
-        for (int i=0; i<size;++i)
-        {
-            mn=min(mn,arr[i]);
-            mx=max(mx,arr[i]);
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = read.nextInt();
+        System.out.println("Enter the elements ");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; ++i)
+            arr[i] = read.nextInt();
+        int mn, mx;
+        mn = mx = arr[0];
+        for (int i = 0; i < size; ++i) {
+            mn = min(mn, arr[i]);
+            mx = max(mx, arr[i]);
         }
-        System.out.println(mx+" "+mn);
+        System.out.print("Max and min elements  ");
+        System.out.println( mx+ " " + mn);
 
     }
-    }
+}
