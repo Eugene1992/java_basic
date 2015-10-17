4ca20674d87084a42187a9d31a02d651c53f3a45
OksanaMikolenko <kseniamikolenko@outlook.com>
2015-10-17 11:52:55 +0200

    hw

--- oksana/src/main/hw/hw5/FifthTask.java
+++ oksana/src/main/hw/hw5/FifthTask.java
@@ -1,7 +1,30 @@
 package hw5;
 
+import java.util.Scanner;
+
 /**
  * Created by Оксана on 16.10.2015.
+ *
+ * Найти сумму все элементов в массиве
+ *
  */
 public class FifthTask {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size =read.nextInt();
+        System.out.println("Enter the elements of array: ");
+        int[] arr = new int[size];
+        for(int i=0; i<size;++i)
+            arr[i] = read.nextInt();
+        int sum=0;
+        for(int i=0; i<arr.length; i++) {
+            sum=sum+arr[i];
+        }
+        System.out.println("The sum of array  "+sum);
+    }
+
+
+
 }
+
