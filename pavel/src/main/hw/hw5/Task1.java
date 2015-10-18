bf61cd6a11998a75df4f9ef0790ae89836275121
potanin.p <Jhkfylj325>
2015-10-18 18:00:45 +0300

    Додано hw5 task2

--- pavel/src/main/hw/hw5/Task1.java
+++ pavel/src/main/hw/hw5/Task1.java
@@ -11,7 +11,6 @@ public class Task1 {
     public static void main(String[] args) {
         int[] myList = {4, 9, 2, 9, 3, 4, 3, 5, 66, 78, 99};
 
-        // Print all the array elements
         for (int i = 0; i < myList.length; i++) {
             if (myList[i] % 2 == 0) {
                 System.out.println("Число " + myList[i] + " четное");
---
+++ pavel/src/main/hw/hw5/Task2.java
@@ -0,0 +1,10 @@
+package hw5;
+
+/**
+ * Created by ppota on 10/18/2015.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        int[] myList = {4, 9, 2, 9, 3, 4, 3, 5, 66, 78, 99};
+
+}
