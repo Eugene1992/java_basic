019378357debd16d68fdf34d9d53c94f8f60bcb5
potanin.p <Jhkfylj325>
2015-10-18 18:23:39 +0300

    Додано hw5 task2

--- pavel/src/main/hw/hw5/Task2.java
+++ pavel/src/main/hw/hw5/Task2.java
@@ -4,7 +4,15 @@ package hw5;
  * Created by ppota on 10/18/2015.
  */
 public class Task2 {
-    public static void main(String[] args) {
-        int[] myList = {4, 9, 2, 9, 3, 4, 3, 5, 66, 78, 99};
 
-}
+        public static void main(String[] args)  {
+            int mass[] = new int[51];
+            for (int i=1; i<11; i++) {
+                mass[i] = (i*2)-1;
+            }
+            System.out.println();
+            for (int i=10; i>=1; i--){
+                System.out.print(mass[i] + " ");
+            }
+        }
+    }
