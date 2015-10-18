fe05d33cb798c26d037fbe740d722da91ae9d379
potanin.p <Jhkfylj325>
2015-10-18 16:51:06 +0300

    Додано hw4 task11

---
+++ pavel/src/main/hw/hw4/Task11.java
@@ -0,0 +1,30 @@
+package hw4;
+
+import java.util.Scanner;
+
+/**
+ * Created by ppota on 10/18/2015.
+ */
+public class Task11 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите натуральное число A");
+        a = scn.nextInt();
+
+        Scanner scn1 = new Scanner(System.in);
+        System.out.println("Введите натуральное число B");
+        b = scn1.nextInt();
+
+
+
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        System.out.println("Результат " +a);
+    }
+
+}
