2a8624b69f842aa90972893326aeffd64826a353
Bokhanchenko <Bokhanchenko.dm@gmail.com>
2015-10-31 12:34:31 +0200

    Home task with Adress is done (Task16)

---
+++ dmitriy/src/main/hw_oop/Schoolboy.java
@@ -0,0 +1,20 @@
+
+public class Schoolboy {
+
+    String name;
+    String surname;
+    int assessment;
+
+    public Schoolboy(String name, String surname, int assessment) {
+        this.name = name;
+        this.surname = surname;
+        this.assessment = assessment;
+    }
+
+    public String toString() {
+        return surname + " " + name + " отримає оцінку " + assessment;
+    }
+
+}
+
+
