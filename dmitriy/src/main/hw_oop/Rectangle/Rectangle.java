f49624afad24f25a022da5c039c3b71a139bcd11
Bokhanchenko <Bokhanchenko.dm@gmail.com>
2015-11-03 18:07:49 +0200

    Home task  Rectangle is done ()

--- dmitriy/src/main/hw_oop/Rectangle/Rectangle.java
+++ dmitriy/src/main/hw_oop/Rectangle/Rectangle.java
@@ -23,11 +23,11 @@ public class Rectangle {
     }
 
     public double areaCalculator(double side1, double side2){
-        double area = (double) side1 * side2;
+        double area = side1 * side2;
         return area;
     }
     public double perimeterCalculator (double side1, double side2){
-        double perimeter = (double)(2 * (side1 + side2));
+        double perimeter = 2 * (side1 + side2);
         return perimeter;
     }
 }
--- dmitriy/src/main/hw_oop/Rectangle/Test.java
+++ dmitriy/src/main/hw_oop/Rectangle/Test.java
@@ -7,12 +7,12 @@ public class Test {
 
         System.out.print("Please, enter the first side of the rectangle - ");
         Scanner scanner = new Scanner(System.in);
-        double side1 = scanner.nextDouble();
+        double side1 = Double.parseDouble(scanner.next());
 
 
         System.out.print("Please, enter the second side of the rectangle - ");
-        double side2 = scanner.nextDouble();
-        Rectangle rectangle = new Rectangle(side1, side2);
+        double side2 = Double.parseDouble(scanner.next());
+        Rectangle rectangle = new Rectangle(side1, side1);
 
         System.out.println("Area is - " + rectangle.areaCalculator(side1,side2));
         System.out.println("Perimeter is - " + rectangle.perimeterCalculator(side1, side2));
