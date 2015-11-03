a95fc320cd610e567e376e235639b85a03949f79
Bokhanchenko <Bokhanchenko.dm@gmail.com>
2015-11-03 18:21:00 +0200

    Home task  Rectangle is done ()

--- dmitriy/src/main/hw_oop/Rectangle/Rectangle.java
+++ dmitriy/src/main/hw_oop/Rectangle/Rectangle.java
@@ -1,7 +1,5 @@
 package Rectangle;
 
-import java.util.Scanner;
-
 public class Rectangle {
 
     private double side1;
@@ -17,17 +15,16 @@ public class Rectangle {
     public void setSide1(double newSide1){side1 = newSide1;}
     public void setSide2(double newSide2){side2 = newSide2;}
 
-    public Rectangle(double side1, double side2){
+    public Rectangle(){
         this.side1 = side1;
         this.side2 = side2;
     }
-
-    public double areaCalculator(double side1, double side2){
-        double area = side1 * side2;
+    public double areaCalculator (double getSide1, double getSide2){
+        double area = getSide1 * getSide2;
         return area;
     }
-    public double perimeterCalculator (double side1, double side2){
-        double perimeter = 2 * (side1 + side2);
+    public double perimeterCalculator (double getSide1, double getSide2){
+        double perimeter = 2 * (getSide1 + getSide2);
         return perimeter;
     }
 }
--- dmitriy/src/main/hw_oop/Rectangle/Test.java
+++ dmitriy/src/main/hw_oop/Rectangle/Test.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
+        Rectangle rectangle = new Rectangle();
+
         System.out.print("Please, enter the first side of the rectangle - ");
         Scanner scanner = new Scanner(System.in);
-        double side1 = Double.parseDouble(scanner.next());
+        rectangle.setSide1(Double.parseDouble(scanner.next()));
 
 
         System.out.print("Please, enter the second side of the rectangle - ");
-        double side2 = Double.parseDouble(scanner.next());
-        Rectangle rectangle = new Rectangle(side1, side1);
+        rectangle.setSide2(Double.parseDouble(scanner.next()));
 
-        System.out.println("Area is - " + rectangle.areaCalculator(side1,side2));
-        System.out.println("Perimeter is - " + rectangle.perimeterCalculator(side1, side2));
+        System.out.println("Area is - " + rectangle.areaCalculator(rectangle.getSide1(), rectangle.getSide2()));
+        System.out.println("Perimeter is - " + rectangle.perimeterCalculator(rectangle.getSide1(), rectangle.getSide2()));
     }
 }
