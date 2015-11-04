cafb941c911d188605120f6b9685eb247ea6dd70
Bokhanchenko <Bokhanchenko.dm@gmail.com>
2015-11-04 14:41:01 +0200

    Home task  "Car2" is done ()

---
+++ dmitriy/src/main/hw_oop/Car2/Car2.java
@@ -0,0 +1,21 @@
+package Car2;
+/**
+ Задание 3
+ Используя Intelij IDEA создать проект, пакет.
+ (Заново!) Создать класс Машина с полями год(int),скорость(double), вес(int) цвет(String).
+ Создать конструктор по умолчанию, конструктор с 1-м параметром, 2-я, 3-я, 4-я.
+ Перегрузить конструкторы.
+ Создать класс Main, в котором создать экземляры класса Машина с разными параметрами.
+ */
+public class Car2 {
+
+    private int year;
+    private double speed;
+    private int weight;
+    private String color;
+
+    public Car2(int year){this.year = year;}
+    public Car2(int year, double speed){this.year = year; this.speed = speed;}
+    public Car2(int year, double speed, int weight){this.year = year; this.speed = speed; this.weight = weight;}
+    public Car2(int year, double speed, int weight, String color){this.year = year; this.speed = speed; this.weight = weight; this.color = color;}
+}
