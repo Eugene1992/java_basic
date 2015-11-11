d629c2e2550066cbfe5fd46a04fcf04b19f49ede
OksanaMikolenko <kseniamikolenko@outlook.com>
2015-11-11 02:41:46 +0200

    hw

---
+++ oksana/src/main/hw_oop/hw4/pdf_task1/Vehicle.java
@@ -0,0 +1,17 @@
+package hw4.pdf_task1;
+
+/**
+ * Created by Оксана on 11.11.2015.
+ */
+public class Vehicle {
+    Vehicle print = new Vehicle();
+    class Wheel{
+        Vehicle.Wheel print = new Wheel();
+
+
+    }
+    class Door{
+        Vehicle.Door print = new Door();
+
+    }
+}
