5481c83264583b5440add6c1e7967f7843572c59
Eugene <deyneko55@gmail.com>
2015-11-28 15:22:40 +0200

    IO homework

---
+++ examples/src/java/colls/IOTest.java
@@ -0,0 +1,12 @@
+package colls;
+
+import org.junit.Test;
+
+public class IOTest {
+
+    @Test
+    public void someTest(){
+        System.out.println((int)'я');
+    }
+
+}
--- examples/src/java/colls/SetExamples.java
+++ examples/src/java/colls/SetExamples.java
@@ -72,4 +72,5 @@ public class SetExamples {
             System.out.println(currentCar);
         }
     }
+
 }
