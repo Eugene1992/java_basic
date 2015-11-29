529a829bbe2eab390058330cca1c8006e51d0c08
ppotanin <1DtlhjC*tckfvb>
2015-11-29 10:07:48 +0200

    class work

---
+++ pavel/src/main/hw/clasWork/IO/ReaderMy.java
@@ -0,0 +1,19 @@
+package clasWork.IO;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+public class ReaderMy extends FileInputStream {
+    public ReaderMy(String name) throws FileNotFoundException {
+        super(name);
+        {
+//            public ReaderMy(File file)throws FileNotFoundException {
+//            super(file);
+//        }
+//            {
+//                public ReaderMy(String name)throws FileNotFoundException {
+//                super(name);
+            }
+            }
+        }
