b6ffb430c11f9f1c20847dc4184d3a9afb1819b4
A-Bush <a.bushlyakov@gmail.com>
2015-11-30 21:21:18 +0200

    examples

--- examples/src/java/regex/RegexExample.java
+++ examples/src/java/regex/RegexExample.java
@@ -10,20 +10,20 @@ public class RegexExample {
     @Test
     public void someRegexTest(){
         // регулярное выражение
-        // любой символ от a-z в нижнем регистре
+        // любой один символ от a-z в нижнем регистре
         Pattern pt1 = Pattern.compile("[a-z]");
 
-        // любой символ от a-z в нижнем и верхнем регистрах
+        // любой один символ от a-z в нижнем и верхнем регистрах
         Pattern pt2 = Pattern.compile("[a-zA-Z]");
 
-        // любой символ от a-z в нижнем и верхнем регистрах включая цифры
+        // любой один символ от a-z в нижнем и верхнем регистрах включая цифры
         Pattern pt3 = Pattern.compile("[a-zA-Z0-9]");
 
         // любой символ от a-z в нижнем и верхнем регистрах включая цифры c определенным количеством повторений
         Pattern pt4 = Pattern.compile("([a-zA-Z0-9]){4}");
 
         // любой символ от a-z в нижнем и верхнем регистрах включая цифры c произвольным количеством повторений
-        Pattern pt5 = Pattern.compile("([a-zA-Z0-9]){1,5}");
+        Pattern pt5 = Pattern.compile("([a-zA-Z0-9]){3,5}");
 
         // любой символ от a-z в нижнем и верхнем регистрах включая цифры c произвольным количеством повторений,
         // кроме символа f в двух регистрах
@@ -35,7 +35,7 @@ public class RegexExample {
 
         // любой символ от a-z в нижнем и верхнем регистрах включая цифры c произвольным количеством повторений,
         // кроме символа f в двух регистрах + заданное количество пробелов в конце строки
-        Pattern pt8 = Pattern.compile("([a-zA-Z0-9&&[^fF]]){1,5}[\\s]{1,10}");
+        Pattern pt8 = Pattern.compile("([a-zA-Z0-9&&[^fF]]){1,5}[\\s]{1,3}");
 
         // любой символ от a-z в нижнем и верхнем регистрах включая цифры c произвольным количеством повторений,
         // кроме символа f в двух регистрах + произвольное количество пробелов в конце строки
@@ -48,10 +48,10 @@ public class RegexExample {
         // любой символ от a-z в нижнем и верхнем регистрах включая цифры c произвольным количеством повторений,
         // кроме символа f в двух регистрах + произвольное количество пробелов + как минимум один символ + возможно(0, 1) точка
         //+ как минимум(1, ) один пробел
-        Pattern pt11 = Pattern.compile("([a-zA-Z0-9&&[^fF]]){1,10}[\\s]{0,}([a-zA-Z0-9&&[^fF]]){1,10}[\\.]{0,1}[\\s]{1,}");
+        Pattern pt11 = Pattern.compile("([a-zA-Z0-9&&[^fF]]){0,1}[\\s]{0,}([a-zA-Z0-9&&[^fF]]){1,10}[\\.]{0,1}[\\s]{1,}");
 
         // проверяемая строка
-        Matcher m = pt11.matcher("hjh asdsa. sdasds sds");
+        Matcher m = pt11.matcher("GT.");
 
         boolean result = m.matches();
 
@@ -70,17 +70,39 @@ public class RegexExample {
                                     + "@(([a-z]){1,}[\\.-]{0,1}([a-z]){1,})+"
                                     + "([\\.]{1}([a-z]){2,4})+");
 
-        //[a-zA-Z0-9] == \\W
-        //[a-zA-Z0-9]{1,} == \\W+
+        //[a-zA-Z0-9_] == \\w
+        //[a-zA-Z0-9]{1,} == \\w+
         //{0,1} == ?
         //()+ == *
 
         Pattern pt3 = Pattern.compile("\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,4})+");
 
-        Matcher m = pt3.matcher("deyneko-email.one@gmail-name.com.ua");
+        Matcher m = pt3.matcher("deyneko-email.onedeyneko-email.one@gmail-name.com.ua.com");
 
         boolean result = m.matches();
 
         System.out.println("email-valid: " + result);
+    }
+
+    @Test
+    public void tailand(){
+        final String TEXT = "Мне очень нравится Тайланд. Таиланд это то место куда бы я поехал. тайланд - " +
+                "мечты сбываются!";
+
+        System.out.println(TEXT.replaceAll("[Тт]а[ий]ланд", "Украина"));
+
+    }
+
+    @Test
+    public void testFile(){
+        final String TEXT = "Username Password";
+
+        String s = "Hello";
+        s = s + " woild!";
+        StringBuilder sb = new StringBuilder(s);
+        sb.append(" world!");
+        String newString = sb.toString();
+
+        System.out.printf("I'm %s years old!", Math.random());
     }
 }
